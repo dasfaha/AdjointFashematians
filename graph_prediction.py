3c7cfe4431a08039e40b5078c430f4e7331c6c4c
Simon Funke <simon.funke@gmail.com>
2013-04-13 21:09:57 +0100

    fix a bug from the previous commit

--- graph_prediction.py
+++ graph_prediction.py
@@ -3,17 +3,18 @@ from math import sqrt
 
 class GraphPredicter():
 
-    def __init__(self):
-        self.tg = TrainingGraph(remove_edges=100)
+    def __init__(self, remove_edges=100):
+        self.tg = TrainingGraph(remove_edges=remove_edges)
 
     def diff(self, a_attr, b):
         d = 0.0
-        for k in self.tg.td.node_attr_map[a].keys():
+        #['follower_count','following_count','listed_count','mentions_received','retweets_received','mentions_sent','retweets_sent','posts','network_feature_1','network_feature_2','network_feature_3']
+        for k in self.tg.td.node_attr_map[b].keys():
             d += (float(a_attr[k])-float(self.tg.td.node_attr_map[b][k]))**2
         return sqrt(d)
 
     def find_closest_neighbour(self, a_attr):
-        dist = [diff(a_attr, b) for b in self.tg.td.nodes] 
+        dist = [self.diff(a_attr, b) for b in self.tg.td.nodes] 
         mindist = min(dist)
 
         for i, d in enumerate(dist):
@@ -21,22 +22,24 @@ class GraphPredicter():
                 return i
 
     def predict(self, a_attr, b_attr):
-        a = find_closest_neighbour(a_attr)
-        b = find_closest_neighbour(b_attr)
+        a = self.find_closest_neighbour(a_attr)
+        b = self.find_closest_neighbour(b_attr)
 
         i = self.tg.get_influence(a, b) > 0
         return i
 
-    def perform_test():
+    def perform_test(self):
         correct = 0
         total = 0
-        for a, b in tg.removed_edges:
-            a_attr = tg.td.node_attr_map[a]
-            b_attr = tg.td.node_attr_map[b]
-            if predict(a_attr, b_attr) > 0:
+        for a, b in self.tg.removed_edges:
+            a_attr = self.tg.td.node_attr_map[a]
+            b_attr = self.tg.td.node_attr_map[b]
+            if self.predict(a_attr, b_attr) > 0:
               correct += 1
             total += 1
 
         print "Success ratio %f" % (float(correct)/total) 
 
-gp = GraphPredicter()
+for i in range(1,10):
+    gp = GraphPredicter(remove_edges=i*10)
+    gp.perform_test()
